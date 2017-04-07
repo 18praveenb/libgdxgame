6594eb997beed90869d694780a28face38f5e502
18praveenb <Praveen@shire.gateway.pace.com>
2017-04-06 21:27:33 -0700

    font files & tile select
    I really need to get the turn system working

--- core/src/com/mygdx/game/GridManager.java
+++ core/src/com/mygdx/game/GridManager.java
@@ -1,7 +1,9 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
@@ -22,17 +24,63 @@ public class GridManager extends Manager {
     }
 
     @Override
+    public void touchUp() {
+        super.touchUp();
+
+
+    }
+
+    @Override
+    public void touchDown() {
+        super.touchDown();
+
+        GridPoint point = selectedGridPoint();
+        int row = point.getY();
+        int col = point.getX();
+
+        if ((col >= level.getNumCols()) || (row >= level.getNumRows()))
+            return;
+
+        level.getTiles()[row][col].setHighlight(Color.RED);
+    }
+
+    @Override
     public void drawFrame() {
         super.drawFrame();
-        getBatch().begin();
+        SpriteBatch batch = getBatch();
+
+        batch.begin();
+
         for (int row = 0; row < level.getNumRows(); row++)
             for(int col = 0; col < level.getNumCols(); col++) {
-                getBatch().draw(level.getTiles()[row][col].getTexture(), col * GRID_SIZE, row * GRID_SIZE);
+
+                int x = col * GRID_SIZE;
+                int y = row * GRID_SIZE;
+
+                // Draw tile
+                Tile tile = level.getTiles()[row][col];
+                batch.draw(tile.getTexture(), x, y);
+
+                // Draw outline
+                Texture outline = level.getTexture("outline.png");
+                batch.setColor(tile.getHighlight());
+                batch.draw(outline, x, y);
+                batch.setColor(Color.WHITE);
+
+                // Draw unit
                 Unit unit = level.getUnits()[row][col];
                 if (unit != null)
-                    getBatch().draw(unit.getTexture(), col * GRID_SIZE, row * GRID_SIZE);
+                    batch.draw(unit.getTexture(), x, y);
         }
-        getBatch().end();
+        batch.end();
+    }
+
+    public GridPoint selectedGridPoint() {
+        return screenToGrid(Gdx.input.getX(), convertY(Gdx.input.getY()));
+    }
+
+    public int convertY(int y) {
+        return Gdx.graphics.getHeight() - 1 - y;
     }
 
     public GridPoint worldToGrid(float worldX, float worldY) {
